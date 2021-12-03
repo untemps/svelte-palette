85a0ce0619ed7e1be6fdf290f155461cb90a8a1b
Vincent Le Badezet <v.lebadezet@untemps.net>
2021-12-03 09:36:32 +0100

    refactor: Refactor useTooltip action (#4)

--- src/__tests__/useTooltip.test.js
+++ src/__tests__/useTooltip.test.js
@@ -7,8 +7,8 @@ import { fireEvent } from '@testing-library/svelte'
 import useTooltip from '../useTooltip'
 
 describe('useTooltip', () => {
-	const target = document.createElement('div')
-	const template = document.createElement('div')
+	const target = _createElement('div')
+	const template = _createElement('div')
 	template.className = 'template'
 	const options = {
 		template,
@@ -18,14 +18,16 @@ describe('useTooltip', () => {
 	it('Adds template to the DOM on mouse enter', async () => {
 		useTooltip(target, options)
 		await fireEvent.mouseEnter(target)
-		expect(target).toContainElement(template)
+		await _sleep()
+		expect(template).toBeVisible()
 	})
 
 	it('Removes template from the DOM on mouse leave', async () => {
 		useTooltip(target, options)
 		await fireEvent.mouseEnter(target)
 		await fireEvent.mouseLeave(target)
-		expect(target).not.toContainElement(template)
+		await _sleep()
+		expect(template).not.toBeVisible()
 	})
 
 	it('Triggers callback on tooltip click', async () => {
