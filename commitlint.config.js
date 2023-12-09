14c3c10b153419b711ec2465c52f761bd38ee5fa
Vincent Le Badezet <vincent@zelty.fr>
2023-12-09 19:30:47 +0100

    chore: Fix commitlint body-max-line-length config

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ export default {
 	rules: {
 		'subject-case': [2, 'always', ['sentence-case']],
 		'scope-case': [2, 'always', ['lower-case', 'upper-case']],
+		'body-max-line-length': [0, 'always', 1000],
 		'footer-max-line-length': [0, 'always', 1000],
 	},
 }
