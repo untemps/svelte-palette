b448720cb318273d54d95b5ff3cc8a5c20cc37bc
untemps <v.lebadezet@untemps.net>
2026-03-27 08:53:07 +0100

    test: Remove requestSubmit test warning by silencing them

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
 	test: {
 		global: true,
 		environment: 'jsdom',
+		onConsoleLog(log, type) {
+			if (type === 'stderr' && log.includes('Not implemented: HTMLFormElement.prototype.requestSubmit')) {
+				return false
+			}
+		},
 		coverage: {
 			reporter: ['text', 'lcov'],
 			exclude: ['src/routes/**', 'svelte.config.js', 'commitlint.config.js'],
--- vitest.setup.js
+++ vitest.setup.js
@@ -1,6 +1,1 @@
 import '@testing-library/jest-dom/vitest'
-
-// jsdom does not implement HTMLFormElement.prototype.requestSubmit
-HTMLFormElement.prototype.requestSubmit = function () {
-	this.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }))
-}
